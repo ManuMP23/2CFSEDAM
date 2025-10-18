2df41a1b282027eb17c4566c1d17fe5bd4250bbe
manut <manutiko23@gmail.com>
2025-10-18 12:53:56 +0200

    Primer commit del proyecto Dice Roller challenge

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,95 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.diceroller.ui.theme.DiceRollerTheme
+import androidx.compose.ui.Alignment
+import androidx.compose.material3.Button
+import androidx.compose.ui.res.stringResource
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DiceRollerTheme {
+                DiceRollerApp()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DiceRollerApp() {
+    DiceWithButtonAndImage(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center))
+}
+@Composable
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    var result1 by remember { mutableStateOf(1) }
+    var result2 by remember { mutableStateOf(1) }  //segunda variable para el segundo dado
+
+    val imageResource1 = when (result1) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
+    val imageResource2 = when (result2) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        //primer dado
+        Image(
+            painter = painterResource(imageResource1),
+            contentDescription = result1.toString()
+        )
+        Button(onClick = { result1 = (1..6).random() }) {
+            Text(stringResource(R.string.roll))
+        }
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        //segundo dado
+        Image(
+            painter = painterResource(imageResource2),
+            contentDescription = result2.toString()
+        )
+        Button(onClick = { result2 = (1..6).random() }) {
+            Text(stringResource(R.string.roll))
+        }
+    }
+}
